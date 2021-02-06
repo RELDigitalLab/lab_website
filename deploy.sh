df469362127cdd552a11d9a6663ccd11b826190b
Jeri Elizabeth <jeri.elizabeth@gmail.com>
2021-02-05 19:43:33 -0600

    update deploy code for new server

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 bundle exec jekyll build 
-rsync -avz --delete _site/ -e "ssh -p 5816" --progress  datalabs@betatesting.as.ua.edu:~/www/
+rsync -avz --delete _site/ -e "ssh -p 5816" --progress  reldigitallab@as.ua.edu:~/www/
